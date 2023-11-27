71e47196fbfe776077b0f057194db6e9cbf7e32b
AnkulKaushik12 <ankul.kaushik@machtigsolutions.com>
2023-11-27 19:31:42 +0530

    few changes in deck field

--- src/component/Drag.js
+++ src/component/Drag.js
@@ -234,6 +234,30 @@ const Drag = () => {
 
   return (
     <>
+    <div className="contain">
+        {val.length==0 && (
+      <div className="welcome-presenter">
+      <div className="welcome-i">
+        <div className='icon'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+          </svg>
+          <h5>Welcome to Deck!</h5>
+        </div>
+        <div className="close-button" >
+          X
+        </div>
+      </div>
+      <div className='welcome-video'>
+        <p>We're glad to have you here and test it out. To get stated, have a look at the video below:</p>
+          <video width="220" height="140" controls>
+          <source  type="video/mp4" />
+          </video>
+      </div>
+
+    </div>
+      )}
       <div className="container">
         <div className="drag-container">
           <div>
@@ -352,6 +376,7 @@ const Drag = () => {
 
         
         {isImported && <ImportedModal handleCloseModal={handleCloseModal} data={{fileId}} />}
+      </div>
       </div>
     </>
   );
--- src/layout/BaseLayout/SideBarLayout/index.js
+++ src/layout/BaseLayout/SideBarLayout/index.js
@@ -150,7 +150,7 @@ const SidebarLayout = (props) => {
         </div>
       </section>
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
